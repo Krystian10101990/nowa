9153e2e64eb450ed2dc444f0cb8219f269ecc1f2
Krystian10101990 <ktuczynski@ur.edu.pl>
2024-07-11 22:16:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-self.addEventListener('install', (event) => {
-    console.log('Service worker zainstalowany');
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('static').then((cache) => {
+        caches.open('dictionary-v1').then(function(cache) {
             return cache.addAll([
                 './',
                 './index.html',
                 './styles.css',
                 './app.js',
+                './manifest.json',
                 './icons/icon-192x192.png',
-                './icons/icon-512x512.png',
+                './icons/icon-512x512.png'
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
     );
